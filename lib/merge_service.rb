eeffa83fcf9cfa67f548c00a60b89e442ac4abf5
Ivan D <dubrovin_ivan_2003@mail.ru>
2025-12-07 12:24:46 +0300

    Add comments in Russian

--- lib/merge_service.rb
+++ lib/merge_service.rb
@@ -34,18 +34,18 @@ class MergeService
     end
   
     def add_merge_notification
-      note_content = "Fusion de l'issue ##{@issue_b.id} dans cette issue. Informations de l'issue B:\n"
-      note_content += "Sujet: #{@issue_b.subject}\n"
-      note_content += "Statut: #{@issue_b.status.name}\n"
-      note_content += "Priorité: #{@issue_b.priority.name}\n"
-      note_content += "Assigné à: #{@issue_b.assigned_to ? @issue_b.assigned_to.name : 'N/A'}\n"
-      note_content += "Auteur: #{@issue_b.author.name}\n"
-      note_content += "Date de création: #{@issue_b.created_on}\n"
-      note_content += "Dernière modification: #{@issue_b.updated_on}\n"
+      note_content = "Слияние задачи ##{@issue_b.id} с данной задачей. Информация по задаче B:\n"
+      note_content += "Тема: #{@issue_b.subject}\n"
+      note_content += "Статус: #{@issue_b.status.name}\n"
+      note_content += "Приоритет: #{@issue_b.priority.name}\n"
+      note_content += "Назначена: #{@issue_b.assigned_to ? @issue_b.assigned_to.name : 'Не назначена'}\n"
+      note_content += "Автор: #{@issue_b.author.name}\n"
+      note_content += "Дата создания: #{@issue_b.created_on}\n"
+      note_content += "Последнее изменение: #{@issue_b.updated_on}\n"
   
       # Ajout des champs personnalisés de l'issue B
       if @issue_b.custom_field_values.any?
-        note_content += "\nChamps personnalisés:\n"
+        note_content += "\nПользовательские поля:\n"
         @issue_b.custom_field_values.each do |custom_field_value|
           field_name = custom_field_value.custom_field.name
           field_value = custom_field_value.value
